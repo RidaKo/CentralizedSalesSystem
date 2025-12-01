2d7fbeb37dd9baddcb141ba2908001e310657e86
Danielius2003 <bdanielius2003@gmail.com>
2025-12-01 11:17:58 +0200

    added DB, still testing

--- CentralizedSalesSystem.API/Program.cs
+++ CentralizedSalesSystem.API/Program.cs
@@ -1,36 +1,31 @@
 using Microsoft.EntityFrameworkCore;
 using CentralizedSalesSystem.API.Data;
 
-
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
 
 builder.Services.AddAuthentication().AddJwtBearer();
 builder.Services.AddAuthorization();
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<CentralizedSalesDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-builder.Services.AddDbContext<CentralizedSalesDbContext>(options =>
+if (!app.Environment.IsDevelopment())
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    app.UseHttpsRedirection();
 }
-);
-
-app.UseHttpsRedirection();
-
 
 app.UseAuthentication();
 app.UseAuthorization();

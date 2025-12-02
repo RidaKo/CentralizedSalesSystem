aa5a57a0ea3e83f319bb4d99eb5a4ee976da26c7
Danielius2003 <bdanielius2003@gmail.com>
2025-12-02 08:06:00 +0200

    deleted some comments

--- CentralizedSalesSystem.API/Program.cs
+++ CentralizedSalesSystem.API/Program.cs
@@ -44,7 +44,6 @@ builder.Services.AddControllers().AddJsonOptions(options =>
     options.JsonSerializerOptions.Converters.Add(
         new System.Text.Json.Serialization.JsonStringEnumConverter());
 });
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 

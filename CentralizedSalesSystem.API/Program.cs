906be4f5431d859f001d456e9c176dc76b669bab
martynaval <martynavalanciute@gmail.com>
2025-12-11 22:26:33 +0200

    fix: Reverted API changes and cleaned up unused variables based on review

--- CentralizedSalesSystem.API/Program.cs
+++ CentralizedSalesSystem.API/Program.cs
@@ -96,8 +96,7 @@ builder.Services.AddCors(options =>
             .WithOrigins(
                 "https://localhost:5001",
                 "https://localhost:7054",
-                "https://localhost:7051",
-                "http://localhost:5251")
+                "https://localhost:7051")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
